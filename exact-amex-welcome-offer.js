7d4ce7ba7664028314c004f32f88f0bedbf91324
Cheng Liu <pwwp@outlook.com>
2026-09-05 22:35:44 -0700

    Update exact-amex-welcome-offer.js

--- exact-amex-welcome-offer.js
+++ exact-amex-welcome-offer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Exact Amex Welcome Offers
 // @namespace    http://tampermonkey.net/
-// @downloadURL    https://raw.githubusercontent.com/pwwpcheng/exact-amex-welcome-offer/refs/heads/main/exact-amex-welcome-offer.js
+// @downloadURL  https://raw.githubusercontent.com/pwwpcheng/exact-amex-welcome-offer/refs/heads/main/exact-amex-welcome-offer.js
 // @updateURL    https://raw.githubusercontent.com/pwwpcheng/exact-amex-welcome-offer/refs/heads/main/exact-amex-welcome-offer.js
 // @version      2.2
 // @description  Displays PZN business card offers and exact personal-card welcome offer details
